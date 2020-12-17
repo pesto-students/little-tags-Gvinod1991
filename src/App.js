3a57a3efadcf54f47782af51759af3fe2a1c69c8
orpitadutta <orpitadutta8@gmail.com>
2020-12-17 18:39:08 +0530

    firebase-integration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React, { useState } from 'react';
-import Modal from './components/modal/Modal'
+import React, { useState, useEffect } from "react";
+import Modal from "./components/modal/Modal";
+import { auth,facebookProvider } from "./services/firebase";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const [show, setShow] = useState(false);
+  const [user, setuser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(async (userIdentity) => {
+      if (userIdentity) {
+        setShow(false);
+        const { displayName, email } = userIdentity;
+        setuser({
+          displayName,
+          email
+        });
+      } else {
+        let pop_status = localStorage.getItem("pop_status");
+        if (!pop_status) {
+          setShow(true);
+          localStorage.setItem("pop_status", 1);
+        }
+      }
+    });
+  }, []);
 
+  console.log("USER", user);
   const closeModalHandler = () => {
     setShow(false);
-  }
+  };
 
   return (
-      <>
-      {show ? <div className="back-drop" onClick={closeModalHandler}></div> : null}
-      <button onClick={() => setShow(true)} className="btn-openModal">Open Modal</button>
+    <>
+      {show ? (
+        <div className="back-drop" onClick={closeModalHandler}></div>
+      ) : null}
       <Modal show={show} close={closeModalHandler} />
     </>
   );
---
+++ src/services/firebase.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+import firebase from "firebase/app";
+import "firebase/auth";
+dotenv.config();
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_MEASUREMENT_ID
+});
+// https://little-tags-78c25-default-rtdb.firebaseapp.com/__/auth/handler
+export const auth = firebase.auth();
+// export const ref = firebase.database().ref();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider()
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+export const signInWithGoogle = () => {
+    auth.signInWithPopup(googleProvider).then((res) => {
+        console.log(res.user)
+    }).catch((error) => {
+        console.log(error.message)
+    })
+}
+
+export const signInWithFacebook = () => {
+    auth.signInWithPopup(facebookProvider).then((res) => {
+        console.log(res.user)
+    }).catch((error) => {
+        console.log(error.message)
+    })
+}
