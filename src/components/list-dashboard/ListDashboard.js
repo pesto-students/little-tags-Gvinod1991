ff2c5ca75e03d3ceddde313ccdf17424dfdd0974
orpitadutta <orpitadutta91@gmail.com>
2020-12-19 20:52:49 +0530

    Code formatting

--- src/components/list-dashboard/ListDashboard.js
+++ src/components/list-dashboard/ListDashboard.js
@@ -1,36 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "./ListDashboard.scss";
 
 const ListDashboard = () => {
-//   const [data, setData] = useState([]);
-//   const [categories, setCategories] = useState([]);
-
-//   const getFilteredCategories = (jsonData) => {
-//     const val = [];
-//     const categoryData = [];
-//     jsonData.filter((el) => {
-//       if (val.indexOf(el.category) === -1) {
-//         val.push(el.category);
-//         categoryData.push({ category: el.category, image: el.image });
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     });
-//     console.log(categoryData);
-//     setCategories(categoryData);
-//   };
-//   useEffect(() => {
-//     fetch("https://fakestoreapi.com/products")
-//       .then((res) => res.json())
-//       .then((json) => {
-//         console.log(json);
-//         setData(json);
-//         getFilteredCategories(json);
-//       });
-//   }, []);
-
   return (
     <>
       <main>
