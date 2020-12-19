9ae1b4a24d6b3e057e7125298405256552b12847
orpitadutta <orpitadutta91@gmail.com>
2020-12-19 08:50:11 +0530

    Merge changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import React, { useState, useEffect } from "react";
-import Modal from "./components/modal/Modal";
-import { auth } from "./services/firebase";
-import Dashboard from'./components/dashboard/Dashboard';
-import ListDashboard from'./components/list-dashboard/ListDashboard';
-import "./App.css";
 import React from "react";
+import "./App.scss";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,45 +10,8 @@ import Cart from './pages/Cart';
 import PrivateRoute from './components/hoc/PrivateRoute';
 
 function App() {
-  const [show, setShow] = useState(false);
-  const [user, setuser] = useState(null);
-  useEffect(() => {
-    auth.onAuthStateChanged(async (userIdentity) => {
-      if (userIdentity) {
-        setShow(false);
-        const { displayName, email } = userIdentity;
-        setuser({
-          displayName,
-          email
-        });
-      } else {
-        let pop_status = localStorage.getItem("pop_status");
-        if (!pop_status) {
-          setShow(true);
-          localStorage.setItem("pop_status", 1);
-        }
-      }
-    });
-  }, []);
-
-  console.log("USER", user);
-  const closeModalHandler = () => {
-    setShow(false);
-  };
 
   return (
-    // <div>
-    //   {show ? (
-    //     <>
-    //     {/* <div className="back-drop" onClick={closeModalHandler}></div> */}
-    //     <Modal show={show} close={closeModalHandler} />
-    //     </>
-    //   ) : null}
-     
-
-    //   <Dashboard />
-    //   <ListDashboard/>
-    // </div>
     <Router>
       <Switch>
         <Route path={'/products/:categoryId'}>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import Modal from "../../components/modal/Modal";
+import { auth } from "../../services/firebase";
+import Dashboard from'../../components/dashboard/Dashboard';
+import ListDashboard from'../../components/list-dashboard/ListDashboard';
 export default function Home(){
+  const [show, setShow] = useState(false);
+  const [user, setuser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(async (userIdentity) => {
+      if (userIdentity) {
+        setShow(false);
+        const { displayName, email } = userIdentity;
+        setuser({
+          displayName,
+          email
+        });
+      } else {
+        let pop_status = localStorage.getItem("pop_status");
+        if (!pop_status) {
+          setShow(true);
+          localStorage.setItem("pop_status", 1);
+        }
+      }
+    });
+  }, []);
+
+  console.log("USER", user);
+  const closeModalHandler = () => {
+    setShow(false);
+  };
+
   return(
     <div>
-      Home screen
+      {show ? (
+        <>
+        {/* <div className="back-drop" onClick={closeModalHandler}></div> */}
+        <Modal show={show} close={closeModalHandler} />
+        </>
+      ) : null}
+     
+
+      <Dashboard />
+      <ListDashboard/>
     </div>
   )
 }
