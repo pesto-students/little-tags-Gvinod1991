c0dd30caa037da9a4d7b179bd1dc69ea4830d17b
Godti Vinod <venkat.godti3@gmail.com>
2020-12-19 11:54:37 +0530

    Mainlayout added to home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import MainLayout from '../../components/layout/MainLayout';
 
-export default function Home(){
-  return(
-    <div>
-      Home screen
-    </div>
-  )
+export default function Home() {
+  return (
+    <MainLayout>
+      <p>Home </p>
+      <p>Home </p>
+      <p>Home </p>
+      <p>Home </p>
+      <p>Home </p>
+      <p>Home </p>
+    </MainLayout>
+  );
 }
