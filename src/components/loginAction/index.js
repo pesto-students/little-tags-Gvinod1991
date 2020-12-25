75f8f3e357f3718dc7a1fa6fc401c7afc49c02ed
Godti Vinod <venkat.godti3@gmail.com>
2020-12-26 00:21:53 +0530

    Login action in header component design

---
+++ src/components/loginAction/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './login-action.scss';
+export default function LoginAction(){
+  return(
+    <div className="login-container">
+      Login/SignUp
+    </div>
+  )
+}
