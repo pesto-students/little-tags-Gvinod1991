da68217b33a91907ed314cd162138e5b9f4d1fd5
Godti Vinod <venkat.godti3@gmail.com>
2021-01-02 00:32:25 +0530

    Choose delivery address  from list implementation and proceed to checkout

--- src/pages/DeliverTo/deliver-to.js
+++ src/pages/DeliverTo/deliver-to.js
@@ -1,33 +1,59 @@
-import React from "react";
-import AddressCard from "../../components/addressCard/addressCard";
-import "./deliver-to.scss";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import AddressCard from '../../components/addressCard/addressCard';
+import MainLayout from '../../components/layout/MainLayout';
+import Loader from '../../components/Loader';
+import { getUserDetailList,saveUserDetailsId } from '../../redux/actions';
+import './deliver-to.scss';
 const DeliverTo = () => {
+  const [errMessage,setErrMessage]=useState(null);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { userDetailsList, loading, userDetailsId } = useSelector((state) => {
+    return { userDetailsList: state.userDetails.userDetailsList, 
+      loading:state.userDetails.loading,
+      userDetailsId:state.userDetails.userDetailsId
+    };
+  });
+  useEffect(() => {
+    dispatch(getUserDetailList());
+  }, [dispatch]);
 
-  const list = 
-  [
-    {
-      name: "Emerson",
-      address : "9522 Libero. St.",
-			city : "Lozzo Atestino",
-			postal :"27476"
-    },
-    {
-      name: "Keane",
-      address : "8857 Tortor Avenue",
-			city : "Cantley",
-			postal :"131189"
+  const redirectNewAddress = () => {
+    history.push('/new-address');
+  };
+  const handleAddressChange =(id)=>{
+    dispatch(saveUserDetailsId(id));
+  }
+  const checkout =()=>{
+    if(userDetailsId){
+      history.push('/checkout')
+    }else{
+      setErrMessage('Choose delivery address to checkout!');
     }
-  ]
+  }
   return (
-    <div className="centerCard">
-      <span><strong>Deliver To</strong></span>
-      {
-        list.map((item,index) => (
-          <AddressCard details = {item} isDummy= {false} />
-        
-          )  )
-      }
-    </div>
+    <MainLayout>
+      <div className="deliver-to-container">
+        <h2>Deliver To</h2>
+        {loading && <Loader />}
+        {userDetailsList &&
+          userDetailsList.length > 0 &&
+          userDetailsList.map((userDetails) => (
+            <AddressCard key={userDetails.id} details={userDetails} isDummy={false} handleAddressChange={handleAddressChange}/>
+          ))}
+        <div className="row-box-deliver-to" onClick={redirectNewAddress}>
+          <img src={'/add.svg'} alt="add new address" />
+          <h5>Add New Address</h5>
+        </div>
+        <button className="btn" onClick={checkout}>
+          <img src="/checkout.svg" alt="shopping cart icon" />
+          <span>Checkout</span>
+        </button>
+        <p className="text-danger">{errMessage}</p>
+      </div>
+    </MainLayout>
   );
 };
 
