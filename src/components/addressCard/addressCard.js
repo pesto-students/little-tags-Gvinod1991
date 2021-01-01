adac50e1b136022cce528ae03d8dc9b0330d2fb9
Godti Vinod <venkat.godti3@gmail.com>
2021-01-02 00:24:24 +0530

    Address card component props update

--- src/components/addressCard/addressCard.js
+++ src/components/addressCard/addressCard.js
@@ -3,23 +3,29 @@ import PropTypes from "prop-types";
 
 import './addressCard.scss';
 
-const AddressCard = ({ details, isDummy }) => {
+const AddressCard = ({ details:{firstName,lastName,emailId,phoneNumber,addressLineOne,addressLineTwo,pinCode,state,id}, isDummy,handleAddressChange }) => {
   return (
         <div className = {"addresscard " + (isDummy ? '' : 'isDummy')}>
           <div className="flex-card">
-           {isDummy? null :<input type="radio" className="form__radio-input" name="address" />}
+           <div className="row-box">
+            {isDummy? null : <input type="radio" className="form__radio-input" name="address" onChange={()=>handleAddressChange(id)} />}
+            <h2>{`${firstName} ${lastName}`}</h2>
+           </div>
               <article className="address">
-              <strong>{details.name}</strong>
-              <br />
-              <span className="addressDetails">{details.address}</span>
-              <span className="addressDetails">{details.city}</span>
-              <span className="addressDetails">{details.postal}</span>
+                <p className="addressDetails">{`${addressLineOne} ${addressLineTwo}`}</p>
+                <p className="addressDetails">{state}</p>
+                <p className="addressDetails">{pinCode}</p>
+                <p></p>
+                <p className="addressDetails">{phoneNumber}</p>
+                <p className="addressDetails">{emailId}</p>
               </article>
           </div>
       </div>
   );
 };
 AddressCard.propTypes = {
-  isDummy: PropTypes.bool.isRequired
+  isDummy: PropTypes.bool.isRequired,
+  details:PropTypes.object.isRequired,
+  handleAddressChange:PropTypes.func.isRequired
 };
 export default AddressCard;
