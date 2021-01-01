b4261c88ce1046446cfdc9d20a491efab8ad1c2b
Godti Vinod <venkat.godti3@gmail.com>
2021-01-02 00:36:10 +0530

    Firebase database import

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,21 +1,23 @@
 import dotenv from 'dotenv'
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 dotenv.config();
 
 firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_ID,
-    measurementId: process.env.REACT_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID
 });
+
 // https://little-tags-78c25-default-rtdb.firebaseapp.com/__/auth/handler
 export const auth = firebase.auth();
-
+export const database= firebase.database();
 export const facebookProvider = new firebase.auth.FacebookAuthProvider()
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
@@ -35,3 +37,4 @@ export const signInWithFacebook = () => {
         console.log(error.message)
     })
 }
+
