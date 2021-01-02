d19841c15cd01442f686f6bcd648fd7fbb9b2ffc
Godti Vinod <venkat.godti3@gmail.com>
2021-01-02 15:25:57 +0530

    Order route update

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import DeliverTo from "./pages/DeliverTo/deliver-to";
 
 import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
+import Orders from "./pages/Orders";
 
 function App() {
 
@@ -41,7 +42,7 @@ function App() {
             <AddAddress />
           </PrivateRoute>
           <PrivateRoute path={'/orders'}>
-            <Cart />
+            <Orders />
           </PrivateRoute>
           <PrivateRoute path={'/checkout'}>
             <Payment /> 
