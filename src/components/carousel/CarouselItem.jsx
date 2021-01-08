c3275f59cb3ae03fc29098c1a0dbef751bf27ead
Godti Vinod <venkat.godti3@gmail.com>
2021-01-08 18:38:36 +0530

    Link to product in carousel add

--- src/components/carousel/CarouselItem.jsx
+++ src/components/carousel/CarouselItem.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-export default function CarouselItem({path,type,title}){
-  return(
-    <div className="carouselItem-container">
-      <img  src={path} alt="carouselItem" />
-      {type==='list' && 
+import PropTypes from 'prop-types';
+export default function CarouselItem({
+  path,
+  type,
+  title,
+  handleCarouselItemClick,
+  id,
+}) {
+  return (
+    <div
+      className="carouselItem-container"
+      onClick={() => (id ? handleCarouselItemClick(id) : {})}
+    >
+      <img src={path} alt="carouselItem" />
+      {type === 'list' && (
         <div className="product-caption">
           <span>{title}</span>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
-CarouselItem.propTypes={
+CarouselItem.propTypes = {
   path: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired
-}
+  type: PropTypes.string.isRequired,
+};
--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './carousel.scss';
 import CarouselItem from './CarouselItem';
-const TRANSLATE_PERCENTAGE=100;
+const TRANSLATE_PERCENTAGE = 100;
 
-export default function Carousel({type,images}) {
-  const sliderArray = images.map(({image,title},index)=>{
-    return <CarouselItem key={index+title} path={image} type={type} title={title} />
-  });
-
+export default function Carousel({ type, images }) {
+  const history = useHistory();
   const [horizontalTranslate, setHorizontalTranslate] = useState(0);
-  const [carouselIndex,setCarouselIndex]=useState(0);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+  const handleCarouselItemClick = (id) => {
+    history.push(`/product/${id}`);
+  };
+  const sliderArray = images.map(({ image, title, id }, index) => {
+    return (
+      <CarouselItem
+        key={index + title}
+        path={image}
+        type={type}
+        title={title}
+        handleCarouselItemClick={handleCarouselItemClick}
+        id={id}
+      />
+    );
+  });
   const goLeft = () => {
-    
     if (horizontalTranslate === 0) {
       setHorizontalTranslate(-TRANSLATE_PERCENTAGE * (sliderArray.length - 1));
-      setCarouselIndex((sliderArray.length - 1));
+      setCarouselIndex(sliderArray.length - 1);
     } else {
       setHorizontalTranslate(horizontalTranslate + TRANSLATE_PERCENTAGE);
-      setCarouselIndex(carouselIndex-1);
+      setCarouselIndex(carouselIndex - 1);
     }
   };
 
@@ -28,41 +40,56 @@ export default function Carousel({type,images}) {
       setCarouselIndex(0);
     } else {
       setHorizontalTranslate(horizontalTranslate - TRANSLATE_PERCENTAGE);
-      setCarouselIndex(carouselIndex+1);
+      setCarouselIndex(carouselIndex + 1);
     }
   };
 
   return (
     <div className="slider">
-      {sliderArray.map((item,index) => {
+      {sliderArray.map((item, index) => {
         return (
           <div
-            key={item+index}
-            className={type==='stack' ? 'slide min-width-100' : 'slide min-width-45' }
+            key={item + index}
+            className={
+              type === 'stack' ? 'slide min-width-100' : 'slide min-width-45'
+            }
             style={{ transform: `translate(${horizontalTranslate}%)` }}
           >
             {item}
           </div>
         );
       })}
-      <button id="left-arrow" className={type === 'stack' ? 'lg' :'sm'} onClick={goLeft}>
-        <img src="/left-arrow.svg" alt="left"/>
+      <button
+        id="left-arrow"
+        className={type === 'stack' ? 'lg' : 'sm'}
+        onClick={goLeft}
+      >
+        <img src="/left-arrow.svg" alt="left" />
       </button>
-      <button id="right-arrow" className={type === 'stack' ? 'lg' :'sm'} onClick={goRight}>
-        <img src="/right-arrow.svg"  alt="right" />
+      <button
+        id="right-arrow"
+        className={type === 'stack' ? 'lg' : 'sm'}
+        onClick={goRight}
+      >
+        <img src="/right-arrow.svg" alt="right" />
       </button>
-      {type === 'stack' && <ul className="carousel-indicators">
-        {sliderArray.map((item,index) => {
-          return <li key={item+index} className={index===carouselIndex ? "active" :""}></li>
-        })
-        }
-      </ul>
-      }
+      {type === 'stack' && (
+        <ul className="carousel-indicators">
+          {sliderArray.map((item, index) => {
+            return (
+              <li
+                key={item + index}
+                className={index === carouselIndex ? 'active' : ''}
+              ></li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
 
-Carousel.propTypes={
-  type:PropTypes.string.isRequired,
-  images:PropTypes.array.isRequired
-}
+Carousel.propTypes = {
+  type: PropTypes.string.isRequired,
+  images: PropTypes.array.isRequired,
+};
