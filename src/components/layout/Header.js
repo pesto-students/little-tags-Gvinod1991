b8e167aff53376fb7c8f010b33d94c5b79ab8d92
Godti Vinod <venkat.godti3@gmail.com>
2021-01-08 18:39:25 +0530

    Hambuger close when click out side

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 import './layout.scss';
 import HamBurgerMenu from './HamBurgerMenu';
 import LoginAction from '../loginAction';
@@ -7,12 +7,14 @@ import Brand from '../brand';
 import AuthUserDisplay from '../autUserDisplay';
 import HeaderCart from '../headerCart';
 import Modal from '../../components/modal/Modal';
+import useOnClickOutside from '../hooks/useOnClickOutside';
 import { isUserVisitingForTheFirstTime } from '../../redux/actions/loginAction';
 
 export default function Header({ isLoggedIn,source='non-home',isSticky, numberOfItemsInCart}) {
   const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
   const [show, setShow] = useState(false);
   const [firstTimeVisit, setFirstTimeVisit] = useState(false);
+  const node= useRef();
   const toggleHamburgerMenu = () => {
     setShowHamburgerMenu(!showHamburgerMenu);
   };
@@ -30,9 +32,10 @@ export default function Header({ isLoggedIn,source='non-home',isSticky, numberOf
     setFirstTimeVisit(isUserVisitingForTheFirstTime() ? true : show);
   }, [show]);
 
+  useOnClickOutside(node, () => setShowHamburgerMenu(false));
   return (
-    <div>
-      <div className={source==='home' && !isSticky ? "header header-fixed" : source==='home' && isSticky ? "header header-fixed header-dark" : "header"}>
+    <div ref={node}>
+      <div  className={source==='home' && !isSticky ? "header header-fixed" : source==='home' && isSticky ? "header header-fixed header-dark" : "header"}>
         <Brand
           toggleHamburgerMenu={toggleHamburgerMenu}
           isLoggedIn={isLoggedIn}
